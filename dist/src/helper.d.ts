b713efe32501c4d9011dfdc1ce0b04d14df03434
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-11-21 23:11:58 +0100

    delete irrelevant files

--- dist/src/helper.d.ts
+++
@@ -1,3 +0,0 @@
-declare function collectWebhooksContent(array: object[]): object[];
-declare function collectEmailsContent(array: object[]): object[];
-export { collectWebhooksContent, collectEmailsContent };
--- src/cypress-importer.js
+++
@@ -1,4 +0,0 @@
-require('Cypress');
-import {getWebhookToken} from '../dist/src/webhookSiteHandler'
-
-Cypress.add.Commands('getWebhookToken', getWebhookToken)
