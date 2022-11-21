aa9947a83af1c9952760aab33779463c62c91c37
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-11-21 23:24:04 +0100

    add cypress command

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -9,6 +9,10 @@ Cypress.Commands.add('getWebhookToken', getWebhookToken);
 declare global {
   namespace Cypress {
     interface Chainable {
+      /**
+       * Custom command to generate Webhook.site token
+       * @example cy.getWebhookToken()
+       */
       getWebhookToken(): Promise<string>;
     }
   }
