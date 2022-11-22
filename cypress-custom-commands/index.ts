d57bec925a2e25069378daa0fdcafdc18d99a24f
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-11-22 20:28:42 +0100

    update

--- cypress-custom-commands/index.ts
+++ cypress-custom-commands/index.ts
@@ -12,28 +12,28 @@ declare global {
       getWebhookToken(): Promise<string>;
       /**
        * Custom command to delete existing webhook token
-       * @example cy.deleteWebhookToken(<tokenId>)
+       * @example cy.deleteWebhookToken('11111111-1111-1111-1111-111111111111')
        */
       deleteWebhookToken(tokenId: string): Promise<Record<string, unknown>>;
       /**
        * Custom command to send payload to Webhook.site
-       * @example cy.sendWebhook(<tokenId>, <payload>)
+       * @example cy.sendWebhook('11111111-1111-1111-1111-111111111111', { id: 1, name: 'Kevin' })
        */
       sendWebhook(tokenId: string, payload: object): Promise<Record<string, unknown>>;
       /**
        * Custom command that retrieves the latest webhook sent to the URL.
-       * @example cy.fetchWebhooksContent(<tokenId>)
+       * @example cy.fetchWebhooksContent('11111111-1111-1111-1111-111111111111')
        */
       fetchLatestWebhookContent(tokenId: string): Promise<object>;
       /**
        * Custom command that retrieves all webhooks sent to the URL.
-       * @example cy.fetchWebhooksContent(<tokenId>)
+       * @example cy.fetchWebhooksContent('11111111-1111-1111-1111-111111111111')
        */
       fetchWebhooksContent(tokenId: string): Promise<object[]>;
       /**
        * Custom command that retrieves all emails sent to the URL.
        * Command can be used only  with cloud verion of Webhook.side service
-       * @example cy.fetchEmailsContent(<tokenId>)
+       * @example cy.fetchEmailsContent('11111111-1111-1111-1111-111111111111')
        */
       fetchEmailsContent(tokenId: string): Promise<object[]>;
     }
