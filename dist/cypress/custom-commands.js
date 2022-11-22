3afef47c84c27c08562bf4792290a347aa0eee7b
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-11-22 18:59:42 +0100

    update

--- dist/cypress/custom-commands.js
+++ dist/cypress/custom-commands.js
@@ -1,5 +1,4 @@
 "use strict";
-/// <reference types="cypress" />
 Object.defineProperty(exports, "__esModule", { value: true });
 const webhookSiteHandler_1 = require("../src/webhookSiteHandler");
 Cypress.Commands.add('getWebhookToken', webhookSiteHandler_1.getWebhookToken);
