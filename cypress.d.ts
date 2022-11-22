13822d6a1ce4ccbed958fa304e68d34701a8d327
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-11-22 01:19:24 +0100

    update

--- cypress.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cypress {
-    interface Chainable {
-      /**
-       * Custom command to generate Webhook.site token
-       * @example cy.getWebhookToken()
-       */
-       getWebhookToken(): Promise<string>;
-    }
-  }
---
+++ cypress/support/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import './commands';
+/// <reference types="cypress" />
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to generate Webhook.site token
+       * @example cy.getWebhookToken()
+       */
+      getWebhookToken(): Promise<string>;
+    }
+  }
+}
