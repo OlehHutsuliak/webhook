eea8b891a8e5f1ebb68a5f272a1548cd3fe579e2
Oleh Hutsuliak <oleh.hutsuliak@kevin.eu>
2022-12-11 12:14:02 +0100

    update

--- src/webhookSiteHandler.ts
+++ src/webhookSiteHandler.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import axiosRetry from 'axios-retry';
+import axiosRetry from 'axios-retry';
 import * as rax from 'retry-axios';
 import { collectWebhooksContent, collectEmailsContent } from './helper';
 
